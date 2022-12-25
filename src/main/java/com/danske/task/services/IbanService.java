aa1ca20e934c169ee3d69366e4714508c1ea5b5e
PineappleOverTheSea <hhalooven@gmail.com>
2022-12-25 12:04:37 +0200

    setup

--- src/main/java/com/danske/task/services/IbanService.java
+++ src/main/java/com/danske/task/services/IbanService.java
@@ -11,16 +11,32 @@ import java.util.stream.Collectors;
 
 @Service
 public class IbanService {
+    private final List<String> countries28 = List.of("AL", "AZ");
     public IbanResponseDto verifyIban(IbanDto ibanDto){
         String iban = ibanDto.getIban().replace(" ", "").toUpperCase();
+        String country = iban.substring(0, 1);
+
+        if(!validateLength(country, iban.length())) {
+            return IbanResponseDto.builder().valid(false).build();
+        }
 
         System.out.println((int) 'A');
         List<Character> ibanList = iban.chars().mapToObj(e -> (char) e).toList();
-        System.out.println(Arrays.asList(iban.toCharArray())) ;
-        return IbanResponseDto.builder().valid(false).build();
+        return IbanResponseDto.builder().valid(true).build();
     }
 
-    private boolean validateLength(){
+    private boolean validateLength(String country, int ibanLength){
+        switch (ibanLength){
+            case (28):{
+                return countries28.contains(country);
+            }
+            case (24):{
 
+            }
+            case (20):{
+
+            }
+            default: return false;
+        }
     }
 }
